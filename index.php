ddcfce13a76b1a8c92f34312ad3b7923d7acd1f9
wayne <iamvaustin@gmail.com>
2017-08-16 13:23:51 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,27 @@
-<?php
-	phpinfo():
+<?
+	require_once("wcrypt/wcrypt.php");
+	$wcrypt = new wcrypt;
+	$a =  $wcrypt->wc_en("php encode example");
 ?>
+<!doctype html>
+<html>
+	<head>
+	<meta charset="utf-8">
+	<title>wcrypt</title>
+		<script src="wcrypt/w_data.js"></script>
+		<script src="wcrypt/wcrypt.js"></script>
+	</head>
+	<body>
+	<script>
+		var strOut = [];
+		window.onload = function(){
+				strOut.push("<?=$a;?>");
+				strOut.push("<?=$wcrypt->wc_de($a);?>");
+				var jsIn = wcrypt_encode("js encode example");
+				strOut.push(jsIn);
+				strOut.push(wcrypt_decode(jsIn));
+				document.body.innerHTML = strOut.join('<br>');
+		};
+		</script>
+	</body>
+</html>
